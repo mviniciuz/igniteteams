69fda92d2fe9c2868f6ebb930c7547ed0ef2e0cd
marcus <you@example.com>
2024-01-23 14:30:10 -0300

    initial project

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+import { styles } from './styles';
+
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
